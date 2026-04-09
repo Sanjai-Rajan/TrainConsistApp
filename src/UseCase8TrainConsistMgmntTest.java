db6123c9ea5f2fb8bd9bf0d883b313ac8e0d94c2
Sanjai Rajan P R <prsanjairajan@gmail.com>
2026-04-09 09:40:51 +0530

    Add documentation to UseCase8TrainConsistMgmntTest
    Added a comment block to the test class for documentation.

--- src/UseCase8TrainConsistMgmntTest.java
+++ src/UseCase8TrainConsistMgmntTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 /**
  * ==========================================================
  * TEST CLASS – UseCase8TrainConsistMgmntTest
